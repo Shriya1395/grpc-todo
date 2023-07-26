cf1f2608e7e18288ec5264fcb3cfd379b2407c08
Shriya Limburkar <slimburkar@secureworks.com>
2023-07-26 16:59:18 +0530

    first commit

---
+++ server/main.go
@@ -0,0 +1,47 @@
+package main
+
+//https://www.golinuxcloud.com/golang-grpc/
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	pb "grpc-todo/proto"
+	"log"
+	"net"
+)
+
+type TodoServer struct {
+	pb.UnimplementedTodoServiceServer
+}
+
+func (s *TodoServer) CreateTodo(ctx context.Context, in *pb.NewTodo) (*pb.Todo, error) {
+	log.Printf("Recived : %v", in.GetName())
+	todo := &pb.Todo{
+		Name: in.GetName(),
+		Description: in.GetDescription(),
+		Done: false,
+		Id: uuid.New().String(),
+	}
+
+	return todo, nil;
+}
+
+func main() {
+	const PORT = ":50051"
+
+	lis, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("Failed connection : ",err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterTodoServiceServer(s, &TodoServer{})
+
+	log.Printf("Server listening at %v ",lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
